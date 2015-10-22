becc2fb718c3613e9df75e02e0cbc18d003ec662
Siyu Sun <sunsiyu.tud@gmail.com>
2015-10-22 16:41:49 +0200

    layoutmat() works now.
    - add assertion
    - special argument condition considered
    - a matrix for layout() argument is returned
    - only equally distributed format is considered right now.

--- R/layout.R
+++ R/layout.R
@@ -11,14 +11,39 @@ scm <- function(x, y) {
   return(x / gcd(x, y) * y)
 }
 
-layoutmat <- function(v, equal=TRUE, ratio=ifelse(equal, rep(1, length(v)), ratio))
+layoutmat <- function(v, equal=TRUE, ratio=equal)
+## TODO: add non-equally distributed condition
 {
+  stopifnot(is.numeric(v))
+  stopifnot(any(v>0))
+  v <- round(v)
+
+  if(length(v) == 1)
+    return(matrix(1:v, nrow=1, byrow=T))
+
   ncol <- 1
   for (i in 1:length(v))
     ncol <- scm(ncol, v[i])
 
-  v <- as.list(v)
+  cum <- cumsum(v)
+  nrep <- ncol/v
+  x <- unlist(mapply(rep, c(0, cum[-length(cum)]), v))
+  y <- unlist(sapply(v, function(x) 1:x))
+  z <- unlist(mapply(rep, nrep, v))
 
-  sapply(v, function(x) rep(x, ncol/x))
+  vmat <- unlist(mapply(function(x,y,z) rep(x+y,z), x, y, z))
+  return(matrix(vmat, nrow=length(v), byrow=T))
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
